208dbc1cd234f7659c2ad948aff86f32c7c01342
Kushan Singh <kushansingh22@gmail.com>
2019-06-12 22:53:48 -0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { from } from 'rxjs';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RouteGuardService } from './service/route-guard.service';
+import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent}, // Conditions on when to activate 
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'todos',component:ListTodosComponent,canActivate:[RouteGuardService]},
   {path:'logout',component:LogoutComponent,canActivate:[RouteGuardService]},
+  {path:'todos/:id',component:TodoComponent,canActivate:[RouteGuardService]}
   {path:'**',component:ErrorComponent},
   
 ];
